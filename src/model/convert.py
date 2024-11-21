4121721a2c28f9ddcc86fe463f1905eb98214e11
m1st-gh <kushpatel169@gmail.com>
2024-11-20 19:26:03 -0500

    Update .gitignore, add labels for testing data, and refactor data processing in convert.py

--- src/model/convert.py
+++ src/model/convert.py
@@ -1,9 +1,10 @@
 import h5py
-import threading
 import csv
-import os
+import scipy.signal as signal
 import numpy as np
-
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
 
 def hdf5_to_dataset(hdf5_file):
     """
@@ -14,7 +15,7 @@ def hdf5_to_dataset(hdf5_file):
     return source_data, source_keys
 
 
-def main():
+def parse_iq():
     hdf5_file = "./data/source_data/2_4ghz_bluetooth.h5"
     output = "./data/convert"
     data, keys = hdf5_to_dataset(hdf5_file)
@@ -31,6 +32,45 @@ def main():
             writer.writerow([idx, "bluetooth"])
     print(f"Saved data_index.csv")
 
+class psd_dataset(Dataset):
+    def __init__(self):
+        self.data_path = "./data/training_data"
+        self.output = "./data/training_data_psd"
+    def __len__(self):
+        return 284
+
+    def __getitem__(self, idx):
+
+        """
+        # Compute the PSD of the input IQ data
+        """
+        data = np.load(f"{self.data_path}/data_{idx}.npy")
+        freq, psd = signal.welch(
+        data, fs=30000000, nperseg=2048, return_onesided=False
+        )
+        np.save(f"{self.output}/data_{idx}.npy", psd)
+        print(f"Saved data_{idx}.npy")
+        return torch.tensor([1])
+
+
+
+def iq_to_psd():
+    data_path = "./data/testing_data"
+    output = "./data/testing_data_psd"
+    """
+    # Compute the PSD of the input IQ data
+    """
+    for i in range(0, 284):
+        data = np.load(f"{data_path}/data_{i}.npy")
+        freq, psd = signal.welch(
+        data, fs=30000000, nperseg=2048, return_onesided=False
+        )
+        np.save(f"{output}/data_{i}.npy", psd)
+        print(f"Saved data_{i}.npy")
+
 
 if __name__ == "__main__":
-    main()
+    dataset = psd_dataset()
+    loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
+    for i, _ in enumerate(loader):
+        pass
